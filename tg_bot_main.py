efaa9c4b0f5f36ea5c084c2073a8fe3331467396
Max Aksenovsky <m.aksenovsky@bk.ru>
2023-03-29 15:21:15 +0300

    generalizing functions

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -6,9 +6,10 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from keyboard import dictionary_of_menu
 from exercises import  *
 from datetime import date
-from FSM import FSMUser, FSMContext
+from FSM import FSMUser, FSMAutomatic, FSMContext
 from connect_to_SQL import connection
 from psycopg2.sql import Literal
+
 
 cursor = connection.cursor()
 TOKEN = '6284880447:AAHj24KgUb7NdxES-aIhXqOerghNbsw6cwM'
@@ -26,51 +27,57 @@ async def start_message(message: types.Message):
     await message.answer(text=help_text)
 
 # выбор тренировки
-
 @dp.message_handler(commands=['new_train'])
-async def type_of_train(message):
+async def type_of_train(message: types.Message, state: FSMContext):
+    await FSMAutomatic.muscular_group.set()
     await message.answer(text=add_new_train_text, reply_markup=dictionary_of_menu['muscular_menu'])
 
 # Хендлер спины и упражнений на спину
-@dp.message_handler(commands=['Спина'])
-async def back(message):
-    await message.answer(text=ex_text, reply_markup=dictionary_of_menu['back_menu'])
+@dp.message_handler(commands=['Спина', 'Грудь', 'Ноги', 'Плечи', 'Руки'], state=FSMAutomatic.muscular_group)
+async def back(message: types.Message, state: FSMContext):
+    async with state.proxy() as helping_data:
+        helping_data['muscular'] = message.text[1:]
+    await message.answer(text=ex_text, reply_markup=dictionary_of_menu[helping_data['muscular']])
+    await state.finish()
     await FSMUser.name_of_exs.set()
 
-@dp.message_handler(commands=back_ex, state=FSMUser.name_of_exs)
-async def f(message: types.Message, state: FSMContext):
+@dp.message_handler(commands=back_ex, state=FSMUser.name_of_exs) ###### ИЗМЕНИТЬ КОМАНДЫ
+async def get_exercise_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data['name_exercise'] = str(message.text[1:])
+        data['name_exercise'] = message.text[1:]
     await FSMUser.next()
     await message.reply(text=data_format)
 
 @dp.message_handler(state=FSMUser.data)
 async def add_data_to_db(message: types.Message, state: FSMContext):
-    today = date.today()
+    """Добавить запись в БД"""
     async with state.proxy() as data:
         data['train_params'] = message.text.split(',')
-        data['date'] = today
+        data['date'] = date.today()
         data['user_id'] = message.from_id
     await state.finish()
     try:
+        # парсинг параметров
         parsed_train_params = '{'
         for i in range(len(data['train_params'])):
             parsed_train_params += data['train_params'][i]
             if i != len(data['train_params']) - 1:
                 parsed_train_params += ', '
         parsed_train_params += '}'
+
         cursor.execute(f"""
         INSERT INTO back_data (user_id, date, exercise, train_params)
         VALUES ({data['user_id']},
                 '{data['date']}', 
                 {Literal(data['name_exercise']).as_string(cursor)},
                 '{parsed_train_params}')""")
+
         connection.commit()
-        await message.answer(text=accepted_text, reply_markup=dictionary_of_menu['main_menu'])
         cursor.close()
         connection.close()
+        await message.answer(text=accepted_text, reply_markup=dictionary_of_menu['main_menu'])
     except Exception as e:
-        await message.answer(text='Ошибка')
+        await message.answer(text=f'Ошибка: {e}. Пиши шефу')
 
 
 
