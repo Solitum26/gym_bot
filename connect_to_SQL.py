852c5059d7cf640c558e9f3dde59bc51f0414c4d
Max Aksenovsky <m.aksenovsky@bk.ru>
2023-03-30 14:19:12 +0300

    deleted some things

--- connect_to_SQL.py
+++ connect_to_SQL.py
@@ -1,10 +1,4 @@
 import psycopg2
 
-connection = psycopg2.connect(
-    database='BackData',
-    host='localhost',
-    user='postgres',
-    password='3008001')
-
 conn = "postgresql://postgres:3008001@localhost/BackData"
 
