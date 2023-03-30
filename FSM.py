d6273ef8e40d6fc31d2db05cbb0b20f028673d2b
Max Aksenovsky <m.aksenovsky@bk.ru>
2023-03-30 14:20:37 +0300

    new class for rewriting data

--- FSM.py
+++ FSM.py
@@ -8,3 +8,7 @@ class FSMUser(StatesGroup):
 
 class FSMAutomatic(StatesGroup):
     muscular_group = State()
+
+class FSMRewriting(StatesGroup):
+    date_and_exercise_to_query = State()
+    new_data_to_update = State()
