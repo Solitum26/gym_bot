ce13d0ad030aa016ab8a9fcde360d4b3920c2432
Max Aksenovsky <m.aksenovsky@bk.ru>
2023-03-31 23:36:21 +0300

    add validation

--- tg_bot_main.py
+++ tg_bot_main.py
@@ -1,3 +1,4 @@
+import prettytable
 import pandas as pd
 import os
 import psycopg2
@@ -9,10 +10,27 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from keyboard import dictionary_of_menu
 from datetime import date
 from FSM import *
-from connect_to_SQL import conn
 from psycopg2.sql import Literal
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv, find_dotenv
+
+def get_connection():
+    connenction = psycopg2.connect(
+                database='user_data',
+                host='localhost',
+                user='postgres',
+                password=f'{password}')
+    return connenction
+
+def validate_exercise_name(text):
+    return text in all_exercises
+
+def validate_params(text: str):
+    try:
+        boolean_flag = ''.join(text.replace('-', ', ').split(', ')).isnumeric()
+        return boolean_flag
+    except:
+        return False
 
 def parsing_data(params_list: list):
     parsed_train_params = '{'
@@ -24,7 +42,7 @@ def parsing_data(params_list: list):
     return parsed_train_params
 
 load_dotenv(find_dotenv())
-
+password = os.getenv('password')
 storage = MemoryStorage()
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot, storage=storage)
@@ -72,33 +90,32 @@ async def add_data_to_db(message: types.Message, state: FSMContext):
     """
     Adding data to PostrgeSQL
     """
-    async with state.proxy() as data:
-        data['train_params'] = message.text.split(', ')
-        data['date'] = date.today()
-        data['user_id'] = message.from_id
-    await state.finish()
-    try:
-        parsed_train_params = parsing_data(data['train_params'])
-        connection = psycopg2.connect(
-            database='BackData',
-            host='localhost',
-            user='postgres',
-            password='3008001')
-        cursor = connection.cursor()
-        cursor.execute(f"""
-        INSERT INTO exercises_data (user_id, date, exercise, train_params)
-        VALUES ({data['user_id']},
-                '{data['date']}', 
-                {Literal(data['name_exercise']).as_string(cursor)},
-                '{parsed_train_params}')""")
-        connection.commit()
-        cursor.close()
-        connection.close()
-        await message.answer(text=accepted_text, reply_markup=dictionary_of_menu['main_menu'])
-    except Exception as e:
-        print(e)
-        await message.answer(text=f'Ошибка: {e}. Свяжитесь с разработчиком',
-                             reply_markup=dictionary_of_menu['muscular_menu'])
+    # Валидация
+    if not validate_params(message.text):
+        await message.answer(text=validate_error_text)
+    else:
+        async with state.proxy() as data:
+            data['train_params'] = message.text.split(', ')
+            data['date'] = date.today()
+            data['user_id'] = message.from_id
+        await state.finish()
+        try:
+            parsed_train_params = parsing_data(data['train_params'])
+            connection = get_connection()
+            cursor = connection.cursor()
+            cursor.execute(f"""
+            INSERT INTO exercises_data (user_id, date, exercise, train_params)
+            VALUES ({data['user_id']},
+                    '{data['date']}', 
+                    {Literal(data['name_exercise']).as_string(cursor)},
+                    '{parsed_train_params}')""")
+            connection.commit()
+            cursor.close()
+            connection.close()
+            await message.answer(text=accepted_text, reply_markup=dictionary_of_menu['main_menu'])
+        except Exception as e:
+            await message.answer(text=f'Ошибка. Свяжитесь с разработчиком',
+                                 reply_markup=dictionary_of_menu['muscular_menu'])
 
 # Хендлер отмены действия
 @dp.message_handler(state='*', commands='cancel')
@@ -113,7 +130,7 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     await message.reply(text='Действие отменено', reply_markup=dictionary_of_menu['main_menu'])
 
 # Хендлер перезаписи ошибчных значнений
-@dp.message_handler(commands=['перезаписать'])
+@dp.message_handler(commands=['rewrite'])
 async def init_state_for_rewriting(message: types.Message):
     """Init 1st state and get data to rewrite"""
     await FSMRewriting.date_and_exercise_to_query.set()
@@ -122,54 +139,85 @@ async def init_state_for_rewriting(message: types.Message):
 @dp.message_handler(state=FSMRewriting.date_and_exercise_to_query)
 async def get_date_and_exercise_query(message: types.Message, state: FSMContext):
     date_and_exercise = message.text.split()
-    async with state.proxy() as new_data:
-        new_data['date'] = date_and_exercise[0][:-1]
-        new_data['exercise'] = date_and_exercise[1]
-    await FSMRewriting.next()
-    await message.reply(text=rewriting_text_new_params)
+    if not validate_exercise_name(date_and_exercise[1]):
+        await message.answer(text='Неправильное название упражнения')
+    else:
+        async with state.proxy() as new_data:
+            new_data['date'] = date_and_exercise[0][:-1]
+            new_data['exercise'] = date_and_exercise[1]
+        await FSMRewriting.next()
+        await message.reply(text=rewriting_text_new_params)
 
 @dp.message_handler(state=FSMRewriting.new_data_to_update)
 async def rewrite_row(message: types.Message, state: FSMContext):
     """Main function for rewriting data"""
-    async with state.proxy() as new_data:
-        new_data['new_params'] = message.text.split(', ')
-    await state.finish()
-    parsed_data = parsing_data(new_data['new_params'])
-    try:
-        connection = psycopg2.connect(
-            database='BackData',
-            host='localhost',
-            user='postgres',
-            password='3008001')
-        cursor = connection.cursor()
-        cursor.execute(f"""
-        UPDATE exercises_data
-        SET train_params = '{parsed_data}'
-        WHERE user_id = {message.from_id} AND date = '{new_data['date']}' AND exercise = '{new_data['exercise']}'""")
-        connection.commit()
-        cursor.close()
-        connection.close()
-        await message.reply(text='Данные обновлены')
-    except Exception as e:
-        print(e)
-        await message.answer(text=f'Ошибка: {e}. Свяжитесь с разработчиком')
+    if not validate_params(message.text):
+        await message.answer(text=validate_error_text)
+    else:
+        async with state.proxy() as new_data:
+            new_data['new_params'] = message.text.split(', ')
+        await state.finish()
+        parsed_data = parsing_data(new_data['new_params'])
+        try:
+            # есть ли в бд записи с заданными параметрами
+            connection = get_connection()
+            cursor = connection.cursor()
+            cursor.execute(f"""
+            SELECT COUNT(*) 
+            FROM exercises_data
+            WHERE user_id = {message.from_id} AND date = '{new_data['date']}' AND exercise = '{new_data['exercise']}'
+            """)
+            if cursor.fetchone()[0]:
+                connection = get_connection()
+                cursor = connection.cursor()
+                cursor.execute(f"""
+                UPDATE exercises_data
+                SET train_params = '{parsed_data}'
+                WHERE user_id = {message.from_id} AND date = '{new_data['date']}' AND exercise = '{new_data['exercise']}'""")
+                connection.commit()
+                cursor.close()
+                connection.close()
+                await message.reply(text='Данные обновлены', reply_markup=dictionary_of_menu['main_menu'])
+            else:
+                await message.answer(text='По заданным параметрам нет данных. Проверьте правильность введенных параметров для обновления')
+        except Exception as e:
+            print(e)
+            await message.answer(text=f'Ошибка. Свяжитесь с разработчиком')
 
 # Хендлер просмотра БД
-@dp.message_handler(commands=['check_history'])
+@dp.message_handler(commands=['history'])
 async def query(message: types.Message):
     try:
         exercise_to_query = message.text.split()[1]
-        df = pd.read_sql_query(f"""
-        SELECT date, train_params
-        FROM exercises_data
-        WHERE exercise = '{exercise_to_query}' AND user_id = {message.from_id}
-        ORDER BY date""", conn, parse_dates=['date'])
-        if not df.empty:
-            await message.answer(df)
+        if not validate_exercise_name(exercise_to_query):
+            await message.answer(text='Неправильное название упражнения')
         else:
-            await message.answer(text='Таблица пуста')
+            from prettytable import from_db_cursor
+            connection = get_connection()
+            cursor = connection.cursor()
+            cursor.execute(f"""
+            SELECT ROW_NUMBER() OVER() as Номер, date as Дата, array_to_string(train_params, ', ') as Параметры
+            FROM exercises_data
+            WHERE exercise = '{exercise_to_query}' AND user_id = {message.from_id}
+            ORDER BY date""")
+            if cursor.rowcount:
+                table = from_db_cursor(cursor)
+                await message.answer(text=f'```{table}```', parse_mode='Markdown')
+            else:
+                await message.answer(text='Таблица пуста')
+    except IndexError as e:
+        await message.answer(text=f'Ошибка. Пожалуйста, введите название упражнения.')
     except Exception as e:
-        print(e)
-        await message.answer(text=f'Ошибка: {e}. Свяжитесь с разработчиком')
+        await message.answer(text='Что-то пошло не так. Перепроверьте запрос или свяжитесь с разработчиком.')
+
+# Получение списка упражнений
+@dp.message_handler(commands=['names'])
+async def return_names(message: types.Message):
+    """Return names of exercises"""
+    text_to_answer = ''
+    for i in all_exercises:
+        text_to_answer += i + '\n'
+    await message.answer(text=text_to_answer)
+
 
 executor.start_polling(dp, skip_updates=True)
